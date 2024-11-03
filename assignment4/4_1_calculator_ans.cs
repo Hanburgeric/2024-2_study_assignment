8148ae3cb90626dddee7f3f3da2064b477fd5520
한원진 <hanweonjin415@gmail.com>
2024-11-04 06:08:25 +0900

    uploaded solution

--- assignment4/4_1_calculator_ans.cs
+++ assignment4/4_1_calculator_ans.cs
@@ -50,7 +50,99 @@ namespace calculator
     public class Calculator
     {
         // ---------- TODO ----------
-        
+
+        public double Calculate(double num1, string op, double num2)
+        {
+            switch (op)
+            {
+                case "+": return num1 + num2;
+                case "-": return num1 - num2;
+                case "*": return num1 * num2;
+                case "/": return num2 != 0.0 ? num1 / num2 : throw new DivideByZeroException("Division by zero is not allowed");
+                default: throw new InvalidOperationException("Invalid operator");
+
+                // CHALLENGE
+                case "**":
+                    {   // Iterative exponentiation by squaring (w/ bit manipulation)
+                        // or just return Math.Pow(num1, (int)num2);
+                        double result = 1.0;
+
+                        // Invert base if exponent is negative
+                        int exponent = (int)num2;
+                        if (exponent < 0)
+                        {
+                            num1 = 1.0 / num1;
+                            exponent = -exponent;
+                        }
+
+                        // Loop through each bit of exponent
+                        while (exponent > 0)
+                        {
+                            // Multiply result by base if its rightmost bit is set
+                            if ((exponent & 1) == 1) result *= num1;
+
+                            // Square base and right shift exponent
+                            num1 *= num1;
+                            exponent >>= 1;
+                        }
+
+                        return result;
+                    }
+
+                case "%":
+                    {   // w/o using modulus operator
+                        // or just return (int)num1 % (int)num2;
+
+                        // Make divisor positive if it is negative
+                        int divisor = (int)num2;
+                        if (divisor == 0) throw new DivideByZeroException("Division by zero is not allowed");
+                        if (divisor < 0) divisor = -divisor;
+
+                        // Perform additions if dividend is negative, subtractions if positive
+                        int dividend = (int)num1;
+                        if (dividend < 0) while (dividend <= -divisor) dividend += divisor;
+                        else while (dividend >= divisor) dividend -= divisor;
+
+                        return (double)dividend;
+                    }
+
+                case "G":
+                    {   // Euclidean algorithm
+                        int a = (int)num1;
+                        if (a < 0) a = -a;
+
+                        int b = (int)num2;
+                        if (b < 0) b = -b;
+
+                        return GCD(a, b);
+                    }
+
+                case "L":
+                    {   // Euclidean algorithm
+                        int a = (int)num1;
+                        if (a < 0) a = -a;
+
+                        int b = (int)num2;
+                        if (b < 0) b = -b;
+
+                        // Special case, LCM(0,0) = 0
+                        if (a == 0 && b == 0) return 0.0;
+                        else return (double)((a / GCD(a, b)) * b);
+                    }
+            }
+        }
+
+        static private int GCD(int a, int b)
+        {   // Euclidean algorithm
+            while (a != 0 && b != 0)
+            {
+                if (a > b) a %= b;
+                else b %= a;
+            }
+
+            return a | b;
+        }
+
         // --------------------
     }
 }
--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -20,7 +20,60 @@ namespace statistics
 
             int stdCount = data.GetLength(0) - 1;
             // ---------- TODO ----------
-            
+
+            var mth_scores = Enumerable.Range(1, stdCount).Select(i => double.Parse(data[i, 2U]));
+            var sci_scores = Enumerable.Range(1, stdCount).Select(i => double.Parse(data[i, 3U]));
+            var eng_scores = Enumerable.Range(1, stdCount).Select(i => double.Parse(data[i, 4U]));
+
+            Console.WriteLine("Average Scores:");
+            Console.WriteLine("Math: " + mth_scores.Average());
+            Console.WriteLine("Science: " + sci_scores.Average());
+            Console.WriteLine("English: " + eng_scores.Average());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Max and min Scores:");
+            Console.WriteLine("Math: (" + mth_scores.Max() + ", " + mth_scores.Min() + ")");
+            Console.WriteLine("Science: (" + sci_scores.Max() + ", " + sci_scores.Min() + ")");
+            Console.WriteLine("English: (" + eng_scores.Max() + ", " + eng_scores.Min() + ")");
+
+            Console.WriteLine();
+
+            // Rank students by score
+            var std_ranks = Enumerable.Range(1, stdCount).Select(i => new
+            {
+                name_ = data[i, 1U],
+                score_ = double.Parse(data[i, 2U]) + double.Parse(data[i, 3U]) + double.Parse(data[i, 4U])
+            }).OrderByDescending(std => std.score_).Select((std, rank) => new
+            {
+                name_ = std.name_,
+                rank_ = rank + 1
+            }).OrderBy(student => student.name_);
+
+            // Convert rank to ordinal number
+            Func<int, string> RankToOrdinal = rank =>
+            {
+                int tens_ones = rank % 100;
+                if (tens_ones == 11 || tens_ones == 12 || tens_ones == 13) return "th";
+                else
+                {
+                    int ones = tens_ones % 10;
+                    switch (ones)
+                    {
+                        case 1: return "st";
+                        case 2: return "nd";
+                        case 3: return "rd";
+                        default: return "th";
+                    }
+                }
+            };
+
+            Console.WriteLine("Student rank by total scores:");
+            foreach (var std in std_ranks)
+            {
+                Console.WriteLine(std.name_ + ": " + std.rank_ + RankToOrdinal(std.rank_));
+            }
+
             // --------------------
         }
     }
